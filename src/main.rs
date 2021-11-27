dcea3a21e93d88493bf92669d196cb0233067b52
J/A <archer884@zoho.com>
2021-11-26 19:54:52 -0600

    Format error message

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-use app::Application;
-use opt::Opts;
-
 mod app;
 mod collector;
 mod error;
 mod opt;
 
+use app::Application;
+use opt::Opts;
+
 type Result<T, E = error::Error> = std::result::Result<T, E>;
 
-fn main() -> Result<()> {
-    Application::new(Opts::from_args()).run()
+fn main() {
+    let opts = Opts::from_args();
+    if let Err(e) = run(opts) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
+}
+
+fn run(opts: Opts) -> Result<()> {
+    Application::new(opts).run()
 }
